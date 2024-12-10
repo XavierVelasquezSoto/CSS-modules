3db8c349c626fd09003fb23cb6dfbfeabe91ffae
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-12-10 18:38:20 +0100

    ..

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,8 @@
 import styles from "./card.module.css"
 
 const Card = ({ img, title, text, buttonText}) => { 
-    const cardBody = `${styles.body + "card-"" }`
-    return <div className={cardBody}>
+    
+    return <div className={styles}>
         <img  className = ""src={img} alt="" />
         <h2 className = "">{title}</h2>
         <span  className = "">{text}</span>
